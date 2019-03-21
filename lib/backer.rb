9665616f5fddc19c70a15d7f2e2c0ff46105ba6b
Mickae Truman <mtruman@nyit.edu>
2019-03-21 01:45:36 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,10 @@
 class Backer
   
-  attr_reader :name, :backed_projects
+  attr_reader :name, :backed_projects, :project
   
   def initialize(name)
     @name = name
     @backed_projects = []
+    @p
   end
 end
